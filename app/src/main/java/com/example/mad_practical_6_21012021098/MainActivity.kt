1ec62d67fdc7b50c48a21ddc778b27d23be8b9d9
Yash <yashpatelv21@gmail.com>
2023-09-21 11:19:04 +0530

    date: 21/09/23

--- app/src/main/java/com/example/mad_practical_6_21012021098/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012021098/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.mad_practical_6_21012021098
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button:Button = findViewById(R.id.button)
-        var button1:Button = findViewById(R.id.button2)
+        var button:FloatingActionButton = findViewById(R.id.button)
+        var button1:FloatingActionButton = findViewById(R.id.button2)
         button.setOnClickListener { playpause() }
         button1.setOnClickListener { stop() }
     }
@@ -20,6 +23,6 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun stop(){
-        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply { startService(this) }
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply { stopService(this) }
     }
 }
